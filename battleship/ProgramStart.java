a006375b039cbca393c6c3fce421fc84fe93aba8
Felixvl <digitaleservice@gmail.com>
2018-05-04 15:30:13 +0200

    2d sea

--- battleship/ProgramStart.java
+++ battleship/ProgramStart.java
@@ -3,8 +3,15 @@ package battleship;
 public class ProgramStart {
 	public static void main(String args[]) {
 		String[] sea = {"x","x","x","x"};
-		for (int x = 0; x < sea.length; x++) {
-			System.out.print(sea[x]);
+		String[] sea1 = {"x","x","x","x"};
+		String[] sea2 = {"x","x","x","x"};
+		String[] sea3 = {"x","x","x","x"};
+		String[][] seas = {sea, sea1, sea2, sea3};
+		for (int x = 0; x < seas.length; x++) {
+			for(int y = 0; y< seas[x].length; y++) {
+				System.out.print(seas[x][y]);
+			}
+			System.out.println("");
 		}
 	}
 
