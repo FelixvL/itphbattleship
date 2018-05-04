e30995d87c053e3a8e6dfc0212297d87a410fa0d
Felixvl <digitaleservice@gmail.com>
2018-05-04 15:51:57 +0200

    go

--- battleship/ProgramStart.java
+++ battleship/ProgramStart.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 
 public class ProgramStart {
 	public static void main(String args[]) {
-		String[] sea =  {"x","x","x","s"};
-		String[] sea1 = {"x","x","x","s"};
-		String[] sea2 = {"s","s","x","s"};
-		String[] sea3 = {"x","x","x","x"};
-		String[][] seas = {sea, sea1, sea2, sea3};
+		String[] sea =  {"D","x","x","x","s"};
+		String[] sea1 = {"C","x","x","x","s"};
+		String[] sea2 = {"B","s","s","x","s"};
+		String[] sea3 = {"A","x","x","x","x"};
+		String[] sea4 = {" ","1","2","3","4"};
+		String[][] seas = {sea, sea1, sea2, sea3, sea4};
 		for (int x = 0; x < seas.length; x++) {
 			for(int y = 0; y< seas[x].length; y++) {
 				System.out.print(seas[x][y]);
